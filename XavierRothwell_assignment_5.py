908b11b6fc26c20b64d10d6d29aaa9f34120b8d1
XavierR2d2 <xwrothwell@aggies.ncat.edu>
2025-10-01 23:27:18 -0400

    Update XavierRothwell_assignment_5.py
    Challenge 2: Prime checker - for loop for known rang

--- XavierRothwell_assignment_5.py
+++ XavierRothwell_assignment_5.py
@@ -11,4 +11,18 @@ while current_number != 1:
         current_number = current_number * 3 + 1
     step_count += 1
 print("1")
+
 print(f"Steps: {step_count}\n")
+
+#Done for Second challenge Commit
+print("=== Challenge 2: Prime Number Checker ===")
+number = int(input("Enter a number: "))
+Number_divisorStart = 2
+Number_divisorEnd = number - 1
+print(f"Testing divisors from {Number_divisorStart} to {Number_divisorEnd}...")
+for divisor in range(Number_divisorStart, Number_divisorEnd + 1):
+    if number % divisor == 0:
+        print(f"{number} is not prime (divisble by {divisor})")
+        break
+else:
+    print(f"{number} is prime!\n")
